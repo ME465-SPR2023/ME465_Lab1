f1d93b773649af4daa0638a81c56c07d639a6807
Cameron Devine <>
2023-02-02 01:57:13 -0800

    added the solution

--- ME465_Lab1/lab1_node.py
+++ ME465_Lab1/lab1_node.py
@@ -8,9 +8,20 @@ class Lab1Node(Node):
     def __init__(self):
         super().__init__("lab1_node")
         self.create_timer(0.05, self.timer_callback)
+        self.vel = self.create_publisher(
+            Twist,
+            "/ctrl_vel",
+            5,
+        )
+        self.start_time = self.get_clock().now().nanoseconds / 1e9
         
     def timer_callback(self):
-        pass
+        msg = Twist()
+        time = self.get_clock().now().nanoseconds / 1e9
+        if time - self.start_time <= 2:
+            msg.linear.x = 0.2
+            msg.angular.z = -0.5
+        self.vel.publish(msg)
         
 
 def main(args=None):
